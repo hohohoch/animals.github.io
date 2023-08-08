2fd83832a075ba0e77229d2c34c48c72c19aa2f8
hohohoch <104590840+hohohoch@users.noreply.github.com>
2023-08-08 14:46:39 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,91 @@
+    //var number  = 13;
+    //var stroka = 'new stroka'
+    
+//if (15 ==5 && number== 13) {
+    //console.log(number);
+//} else {console.log('else');}
+
+//switch (stroka) {
+    ////case "4":console.log('4'); break;
+    //case "new strok":console.log('newstroka'); break;
+    //case "6":console.log(number); break;
+    //default:console.log('default');}
+//var arr = [true, 12, 'gav'];
+//arr[2]=false;
+//console.log(arr.length);
+//console.log(arr);
+//var matrix = [[4,6,8],["stroka",5.7], [0,0.7]];
+//matrix[1][0]=90;
+//console.log(matrix);
+//for(var i = 0; i < matrix.length; i++) {console.log(i);}\
+// var loh = 12;
+// while (loh <= 100) {console.log(loh); loh++;}
+// var ihc = true;
+
+// for(var i=10; i<=20; i++) {
+//     if(i % 2 == 0 )
+//     continue;
+
+
+//     console.log(i);
+// }
+// var arr = [5, 7, 3, 8,  9, "stroka"];
+// for (var i = 0; i < arr.length; i++) {
+//     console.log((i + 1)+ ':' + arr[i]);
+// }
+
+// alert('');
+// var data = confirm('Are you sure?');
+// if (data == true) {
+//     console.log('yes');
+//     document.body.style.backgroundColor = "#AA0000";
+//     alert('Your Phone has been blocked ');
+// }else {console.log('no');
+// alert('Your Phone has been blocked ');
+// document.body.style.backgroundColor = "#AA0000";
+// }
+// var date =prompt('how old are u?',);
+
+// if (date== null) {
+//     console.log('cancel');
+// }else {console.log(date);}
+// var person = null;
+// if(confirm('are u sure?')) {
+// person = prompt('what is your name?'); 
+// alert('hello '+ person);
+// console.log(true);
+// }else {
+// alert('canceled');
+// console.log(false);
+// }
+// function info(word) {
+//     console.log(word + '?');
+// }
+// function sum(a,b){
+//     var res  = a + b;
+//     console.log (res);
+//     info(res);
+// }
+// sum(1,2);
+// function summa(arr){
+//     var sum = 0;
+
+//     for(var i =0; i < arr.length; i++)
+//         sum += arr[i];
+
+//         console.log(sum);
+    
+// }
+// var array = [1,2,3,4,5,6,7,8,9];
+// var array2 = [1,2,3,4,7,8,9];
+// var array3 = [4,5,6,7,8,9];
+// summa(array);
+// summa(array2);
+// summa(array3);
+screen.width
+screen.height
+screen.colorDepth
+console.log(screen.colorDepth);
+console.log(screen.width);
+console.log(screen.height);
+
